471026771b2bd8feda4ca9f60a463becf1480a82
VALTECH-COM\bob.sanders <bob.sanders@valtech.com>
2025-04-03 18:17:28 +0200

    Moved project and included missing unittest project

---
+++ DigitSequenceHelper.Tests/Analysers/GeoMetricNumberAnalyserTests.cs
@@ -0,0 +1,123 @@
+﻿using DigitSequenceHelper.Analysers;
+
+namespace DigitSequenceHelper.Tests.Analysers
+{
+    [TestClass]
+    public class GeoMetricNumberAnalyserTests
+    {
+        #region GCD
+
+        [TestMethod]
+        public void GCD_WithValidNumbers_ReturnsCorrectGCD()
+        {
+            // Arrange
+            var numbers = new List<int> { 3, 9, 27, 81 };
+
+            // Act
+            int? result = GeoMetricNumberAnalyser.GCD(numbers);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void GCD_WithValidNumbersReverse_ReturnsCorrectGCD()
+        {
+            // Arrange
+            var numbers = new List<int> { 81, 27, 9, 3 };
+
+            // Act
+            int? result = GeoMetricNumberAnalyser.GCD(numbers);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void GCD_WithInvalid_ReturnsOne()
+        {
+            // Arrange
+            var numbers = new List<int> { 4, 8, 9 };
+
+            // Act
+            int? result = GeoMetricNumberAnalyser.GCD(numbers);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void GCD_WithSingleNumber_ReturnsSameNumber()
+        {
+            // Arrange
+            var numbers = new List<int> { 3 };
+
+            // Act
+            int? result = GeoMetricNumberAnalyser.GCD(numbers);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void GCD_EmptyList_ReturnsNull()
+        {
+            // Arrange
+            var numbers = new List<int> { };
+
+            // Act
+            int? result = GeoMetricNumberAnalyser.GCD(numbers);
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        #endregion
+
+        #region GetExponent
+
+        [TestMethod]
+        public void GetExponent_WithBase3AndNumber9_Returns2()
+        {
+            // Arrange
+            int baseValue = 3;
+            int number = 9;
+
+            // Act
+            int result = GeoMetricNumberAnalyser.GetExponent(baseValue, number);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GetExponent_WithBase3AndNumber27_Returns3()
+        {
+            // Arrange
+            int baseValue = 3;
+            int number = 27;
+
+            // Act
+            int result = GeoMetricNumberAnalyser.GetExponent(baseValue, number);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void GetExponent_WithBase3AndNumber10_ReturnsNegative1()
+        {
+            // Arrange
+            int baseValue = 3;
+            int number = 10;
+
+            // Act
+            int result = GeoMetricNumberAnalyser.GetExponent(baseValue, number);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        #endregion
+    }
+}
---
+++ DigitSequenceHelper.Tests/Integration/IntegrationTests.cs
@@ -0,0 +1,98 @@
+﻿using DigitSequenceHelper.Analysers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitSequenceHelper.Tests.Integration
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void Integration_Additive_AddingWith2()
+        {
+            // Arrange
+            var sequence = new List<double> { 2, 4, 6, 8 };
+
+            // Act
+            var results = Configuration.Analysers.Select(a => {
+                return a.Analyze(sequence);
+            });
+            var matchedResult = results.FirstOrDefault(x => x.PredictedNumber != null);
+
+            // Assert
+            Assert.AreEqual(10, matchedResult!.PredictedNumber);
+            Assert.AreEqual(new AdditionNumberAnalyser().OperationName, matchedResult.Analyser.OperationName);
+        }
+
+        [TestMethod]
+        public void Integration_Subtractive_SubtractingWith2()
+        {
+            // Arrange
+            var sequence = new List<double> { 10, 8, 6, 4 };
+
+            // Act
+            var results = Configuration.Analysers.Select(a => {
+                return a.Analyze(sequence);
+            });
+            var matchedResult = results.FirstOrDefault(x => x.PredictedNumber != null);
+
+            // Assert
+            Assert.AreEqual(2, matchedResult!.PredictedNumber);
+            Assert.AreEqual(new SubtractionNumberAnalyser().OperationName, matchedResult.Analyser.OperationName);
+        }
+
+        [TestMethod]
+        public void Integration_Multiplicative_MultipliedBy2()
+        {
+            // Arrange
+            var sequence = new List<double> { 10, 20, 40 };
+
+            // Act
+            var results = Configuration.Analysers.Select(a => {
+                return a.Analyze(sequence);
+            });
+            var matchedResult = results.FirstOrDefault(x => x.PredictedNumber != null);
+
+            // Assert
+            Assert.AreEqual(80, matchedResult!.PredictedNumber);
+            Assert.AreEqual(new MultiplicationNumberAnalyser().OperationName, matchedResult.Analyser.OperationName);
+        }
+
+        [TestMethod]
+        public void Integration_Division_DividedBy2()
+        {
+            // Arrange
+            var sequence = new List<double> { 120, 60, 30 };
+
+            // Act
+            var results = Configuration.Analysers.Select(a => {
+                return a.Analyze(sequence);
+            });
+            var matchedResult = results.FirstOrDefault(x => x.PredictedNumber != null);
+
+            // Assert
+            Assert.AreEqual(15, matchedResult!.PredictedNumber);
+            Assert.AreEqual(new DivisionNumberAnalyser().OperationName, matchedResult.Analyser.OperationName);
+        }
+
+        [TestMethod]
+        public void Integration_GeometricSequence_PowerOf3()
+        {
+            // Arrange
+            var sequence = new List<double> { 1, 4, 13, 40 };
+
+            // Act
+            var results = Configuration.Analysers.Select(a => {
+                return a.Analyze(sequence);
+            });
+            var matchedResult = results.FirstOrDefault(x => x.PredictedNumber != null);
+
+            // Assert
+            Assert.AreEqual(121, matchedResult!.PredictedNumber);
+            Assert.AreEqual(new GeoMetricNumberAnalyser().OperationName, matchedResult.Analyser.OperationName);
+        }
+    }
+}
